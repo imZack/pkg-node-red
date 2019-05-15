a99894bcdcf21c491f93450b12444b0e69419079
Zack YL Shih <zackyl.shih@moxa.com>
2019-05-14 23:15:35 -0700

    Update

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -x
 env
 uname -a
 
+npm config set unsafe-perm true
+npm install -g pkg
 npm install "node-red@$NODERED_VERSION"
 node -e "const data = require('./node_modules/node-red/package.json'); data.pkg = { assets: ['./**/*'] }; data.dependencies['node-red-contrib-modbus'] = '4.1.3'; require('fs').writeFileSync('new-package.json', JSON.stringify(data, null, ' '));"
 cp new-package.json node_modules/node-red/package.json
