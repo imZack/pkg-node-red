6e4b32d1eaf913c34d5ee1b2177a91830be55f3f
Zack YL Shih <zackyl.shih@moxa.com>
2019-05-14 23:05:28 -0700

    Update

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 set -x
+env
+uname -a
 
 npm install "node-red@$NODERED_VERSION"
-node -e "const data = require('./node_modules/node-red/package.json'); data.pkg = { assets: ['./**/*'] }; require('fs').writeFileSync('new-package.json', JSON.stringify(data, null, ' '));"
+node -e "const data = require('./node_modules/node-red/package.json'); data.pkg = { assets: ['./**/*'] }; data.dependencies['node-red-contrib-modbus'] = '4.1.3'; require('fs').writeFileSync('new-package.json', JSON.stringify(data, null, ' '));"
 cp new-package.json node_modules/node-red/package.json
 (
   cd node_modules/node-red || exit 1
   npm install --production
   pkg \
-    --targets node8-linux-armv7,node8-linux-x64,node8-win-x64,node8-macos-x64,node10-linux-armv7,node10-linux-x64,node10-win-x64,node10-macos-x64 \
+    --targets "$TARGET" \
     --out-path ../../release --public .
 )
