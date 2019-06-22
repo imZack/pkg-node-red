8be2e0466be6cea732172dd05977f327e72dc342
YuLun Shih <shih@yulun.me>
2019-06-21 23:16:01 -0700

    Fix tp-userprog.sh

--- tp-userprog.sh
+++ tp-userprog.sh
@@ -5,7 +5,7 @@ env
 
 mkdir userprog
 cp -r example/* userprog
-cp "$OUTPUT_DIR/node-red_linux-$ARCH" userprog/node-red
+cp "$OUTPUT_DIR/node-red" userprog/node-red
 chmod +x userprog/node-red
 cd userprog|| exit 1;
 tar czvf "$OUTPUT_DIR/tp-node-red-$ARCH.tar.gz" .
