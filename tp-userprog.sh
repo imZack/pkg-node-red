b281709947150a69e6e3504e61732fe8c8278092
YuLun Shih <shih@yulun.me>
2019-06-21 23:05:39 -0700

    Add ThingsPro User Program

--- tp-userprog.sh
+++ tp-userprog.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-set -x 
+set -xe
+env
 
-## Create armv7
-(
-  mkdir userprog-armv7
-  cp -r example/* userprog-armv7
-  cp release/node-red-linux-armv7 userprog-armv7/node-red
-  chmod +x userprog-armv7/node-red
-  cd userprog-armv7 || exit 1;
-  tar czvf ../release/tp-node-red-armv7.tar.gz .
-)
-
-## Create x64
-(
-  mkdir userprog-x64
-  cp -r example/* userprog-x64
-  cp release/node-red-linux-x64 userprog-x64/node-red
-  chmod +x userprog-x64/node-red
-  cd userprog-x64 || exit 1;
-  tar czvf ../release/tp-node-red-x64.tar.gz .
-)
+mkdir userprog
+cp -r example/* userprog
+cp "$OUTPUT_DIR/node-red_linux-$ARCH" userprog/node-red
+chmod +x userprog/node-red
+cd userprog|| exit 1;
+tar czvf "$OUTPUT_DIR/tp-node-red-$ARCH.tar.gz" .
